bf2c3f387ed46971c19073fd6e2749ea99d7a9ec
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-10-06 15:40:44 +0300

    creates a create superuser testcase method

--- Backend/Trackerapp/tests.py
+++ Backend/Trackerapp/tests.py
@@ -25,3 +25,21 @@ class UsersManagersTests(TestCase):
             User.objects.create_user(email='')
         with self.assertRaises(ValueError):
             User.objects.create_user(email='', password="trackerapp123")
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(email='wambua@g.com', password='projecttracker123')
+        self.assertEqual(admin_user.email, 'wambua@g.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        try:
+            # username is None for the AbstractUser option
+            # username does not exist for the AbstractBaseUser option
+            self.assertIsNone(admin_user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email='wambua@g.com', password='thatsmycar', is_superuser=False)
+    
