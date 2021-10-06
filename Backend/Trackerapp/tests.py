9f7525aa22a5e230ec9f9f66ca4df4e477cbdc58
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-10-06 15:37:02 +0300

    creates a test case for create user

--- Backend/Trackerapp/tests.py
+++ Backend/Trackerapp/tests.py
@@ -7,8 +7,21 @@ class UsersManagersTests(TestCase):
     
     def test_create_user(self):
         User = get_user_model()
-        user = User.objects.create_user(email='freddy@g.com', password = 'drovehomesave')
-        self.assertEqual(user.email, 'freddy@g.com')
+        user = User.objects.create_user(email='fredrick@g.com', password = 'trackerapp123')
+        self.assertEqual(user.email, 'fredrick@g.com')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
+
+        try:
+            # username is None for the AbstractUser option
+            # username does not exist for the AbstractBaseUser option
+            self.assertIsNone(user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email='')
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email='', password="trackerapp123")
