48f75ba9125f4bf41600f4e56f8f959814a7ea64
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-10-06 14:04:44 +0300

    creates usermanager class

--- Backend/Trackerapp/models.py
+++ Backend/Trackerapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+
+        if username is None:
+            raise ValueError('Users must have a username')
+
+        if email is None:
+            raise ValueError('Users must have an email')
+
+        user=self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password=None):
+
+        if password is None:
+            raise ValueError('Password should not be none')
+
+        user=self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.is_active = True
+        user.save()
+        return user
