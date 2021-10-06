57b0d9d167878ef0c3f7dc9a7a960b0455165903
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-10-06 22:46:25 +0300

    creates a signup form

--- Backend/Trackerapp/forms.py
+++ Backend/Trackerapp/forms.py
@@ -16,3 +16,18 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email',)
+
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(max_length=30, required=False)
+    email = forms.EmailField(max_length=255, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = CustomUser
+        fields = ('username','email', 'password1', 'password2')
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
