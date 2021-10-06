aee261100fa1b49b7109384bece132a89f51efd2
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-10-06 14:08:07 +0300

    creates Customuser model

--- Backend/Trackerapp/models.py
+++ Backend/Trackerapp/models.py
@@ -27,3 +27,20 @@ class UserManager(BaseUserManager):
         user.is_active = True
         user.save()
         return user
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, unique=True, db_index=True)
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
