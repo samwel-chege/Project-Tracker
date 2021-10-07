b85e4ef4734d31b6a420a8bdb59341de1a9173a3
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-10-07 20:31:29 +0300

    creates a token

--- Backend/Trackerapp/views.py
+++ Backend/Trackerapp/views.py
@@ -5,6 +5,7 @@ from rest_framework import generics, serializers,status
 from .models import CustomUser
 from .serializers import RegisterSerializer
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken, Token
 
 # Create your views here.
 class RegisterView(generics.GenericAPIView):
@@ -16,7 +17,8 @@ class RegisterView(generics.GenericAPIView):
         serializer =self.serializer_class(data=user)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-
-
+        
         user_data =serializer.data
+        user = CustomUser.objects.get(email=user_data('email'))
+        token = RefreshToken.for_user(user)
         return Response(user_data, status=status.HTTP_201_CREATED)
