b847a1c7e2ab175d93f08bcb1af981c3a45b573b
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-10-07 11:55:18 +0300

    Preparation for merge

--- Backend/project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from django.urls import path, include
-from django.contrib import admin
-import django.contrib.auth.views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
