34527d143c5aa31e4c752b1b99c78cad6bd1c394
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-10-08 00:02:23 +0300

    adds to_mail

--- Backend/Trackerapp/utils.py
+++ Backend/Trackerapp/utils.py
@@ -5,5 +5,5 @@ class Util:
     def send_email(data):
 
         email = EmailMessage(
-            subject=data['email_subject'], body=data['email_body'])
+            subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
         email.send()
