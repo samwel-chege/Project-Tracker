005f9607897fd1fd2219000ef662c2e5a65d0b12
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-10-07 11:30:04 +0300

    creates validate data and create user methods in the register class serializer

--- Backend/Trackerapp/serializers.py
+++ Backend/Trackerapp/serializers.py
@@ -7,3 +7,15 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         field = ['email', 'username', 'password']
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        username = attrs.get('username', '')
+
+        if not username.isalnum():
+            raise serializers.ValidationError('The username should only contain alphanumeric characters')
+
+        return attrs
+    
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
