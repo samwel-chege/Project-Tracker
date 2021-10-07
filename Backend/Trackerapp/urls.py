73533e37f134a6fc6c406c80ebbba50d4afe78e0
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-10-07 21:06:59 +0300

    registers VerifyEmail class view

--- Backend/Trackerapp/urls.py
+++ Backend/Trackerapp/urls.py
@@ -1,12 +1,11 @@
 from django.urls import include, path
-from .views import RegisterView
+from .views import RegisterView, VerifyEmail
 from django.contrib.auth.views import LoginView, LogoutView
 from rest_framework import views
 
 urlpatterns = [
-    path('register', RegisterView.as_view(), name='register')
+    path('register', RegisterView.as_view(), name='register'),
+    path('email-verify/', VerifyEmail.as_view(), name='email-verify')
 
-    # path('signup/', views.signup, name='signup'),
-    # path('login/', LoginView.as_view(), name='login'),
-    # path('logout/', LogoutView.as_view(), name='logout')
+
 ]
