77ab55f7cfb517e05579edb5c437c28aa5ef7f21
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-10-11 17:47:07 +0300

    sets serializer class for password reset request email

--- Backend/Trackerapp/serializers.py
+++ Backend/Trackerapp/serializers.py
@@ -5,6 +5,7 @@ from .models import CustomUser
 from django.contrib import auth
 from rest_framework.exceptions import AuthenticationFailed
 
+
 class RegisterSerializer(serializers.ModelSerializer):
     password=serializers.CharField(max_length=68, min_length=6, write_only=True)
 
@@ -63,4 +64,31 @@ class LoginSerializer(serializers.ModelSerializer):
             'tokens': user.tokens(),
         }
 
+        return super().validate(attrs)
+
+
+class ResetPasswordEmailRequestSerializer(serializers.Serializer):
+    email=serializers.EmailField(min_length=3)
+
+
+    class Meta:
+        fields = ['email']  
+
+    def validate(self, attrs):
+        email = attrs['data'].get('email', '')
+        if CustomUser.objects.filter(email=email).exists():
+            user = CustomUser.objects.get(email=email)
+            uidb64=urlsafe_base64_encode(user.id)
+            token = PasswordResetTokenGenerator().make_token(user)
+            current_site=get_current_site(
+                request=attrs['data'].get('request')).domain
+            relativeLink=reverse(
+                'password-reset-confirmation', kwargs={'uidb64': uidb64, 'token': token})
+            absoluteurl = 'http://'+current_site+relativeLink
+            email_body='Hello, \n Click on the link below to reset your password  \n'+ absoluteurl
+            data={'email_body':email_body,'to_email':user.email, 'email_subject':'Reset your password'}
+
+            Util.send_email(data)
+            
+            return attrs
         return super().validate(attrs)
