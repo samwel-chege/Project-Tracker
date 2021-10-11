008fc2d825e01ff7ca0bf40d943ef96b2d50f1cf
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-10-11 22:03:10 +0300

    registers logout url

--- Backend/Trackerapp/urls.py
+++ Backend/Trackerapp/urls.py
@@ -1,12 +1,15 @@
 from os import name
 from django.urls import include, path
-from .views import RegisterView, VerifyEmail, LoginAPIView, PasswordTokenCheckAPI, RequestPasswordResetEmail, SetNewPasswordAPIView
+from .views import (
+     RegisterView, VerifyEmail, LoginAPIView, PasswordTokenCheckAPI, 
+     RequestPasswordResetEmail, SetNewPasswordAPIView, LogoutAPIView)
 from rest_framework import views
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
+    path('logout/', LogoutAPIView.as_view(), name='logout'),
     path('email-verify/', VerifyEmail.as_view(), name='email-verify'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('request-reset-email/', RequestPasswordResetEmail.as_view(), name='request-reset-email'),
