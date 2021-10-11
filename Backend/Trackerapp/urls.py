be0ecd9f17f2edd5c0b0093b3b58225d301f7be1
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-10-11 13:06:57 +0300

    creates token refresh endpoint

--- Backend/Trackerapp/urls.py
+++ Backend/Trackerapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from .views import RegisterView, VerifyEmail, LoginAPIView
-from django.contrib.auth.views import LoginView, LogoutView
 from rest_framework import views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
-    path('email-verify/', VerifyEmail.as_view(), name='email-verify')
+    path('email-verify/', VerifyEmail.as_view(), name='email-verify'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 
 ]
