dedb560f9f8bfb9d80a762a5129b7f92ddc1708b
samwel-chege <samwelchegeh09@gmail.com>
2021-10-13 00:17:14 +0300

    solve bootstrap import error

--- Frontend/project-tracker/src/app/app.module.ts
+++ Frontend/project-tracker/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ProjectService } from './services/project.service';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module'; 
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
--- Frontend/project-tracker/src/app/services/auth.service.ts
+++ Frontend/project-tracker/src/app/services/auth.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpInterceptor,HttpRequest,HttpHandler,HttpEvent } from '@angular/common/http';
+import { CanActivate, Router } from '@angular/router';
+
+import { Observable } from 'rxjs';
+import { tap, shareReplay } from 'rxjs/operators';
+
+import * as jwtDecode from 'jwt-decode';
+import * as moment from 'moment';
+
+import { environment } from 'src/environments/environment';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private apiRoot = 'http://localhost:8000/auth/';
+
+  constructor(private http: HttpClient) { }
+
+  // private setSession(authResult:any){
+  //   const token = authResult.token;
+  //   const payload = <JWTPayload> jwtDecode(token);
+  // }
 }
