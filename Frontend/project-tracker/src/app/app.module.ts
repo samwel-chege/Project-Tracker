b05616a096cce2bdf00ed8190cdc8a422d88777a
samwel-chege <samwelchegeh09@gmail.com>
2021-10-12 12:36:25 +0300

    create signup component

--- Frontend/project-tracker/src/app/app.module.ts
+++ Frontend/project-tracker/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
