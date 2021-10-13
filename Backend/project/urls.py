c6723a8a99a13ab93edcad243d9c6ad581299086
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-10-13 20:17:12 +0300

    adds url for importing api to postman

--- Backend/project/urls.py
+++ Backend/project/urls.py
@@ -44,6 +44,7 @@ urlpatterns = [
     url(r'', include('Trackerapp.urls')),
 
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/api.json/', schema_view.without_ui(cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
