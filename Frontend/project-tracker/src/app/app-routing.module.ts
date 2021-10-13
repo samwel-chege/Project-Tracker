a5b7c63ba01bb2da7219c130c0bd1004d6d26c48
LOVINE <cynthia.lovine@student.moringaschool.com>
2021-10-13 15:25:27 +0300

    modified files

--- Frontend/project-tracker/src/app/app-routing.module.ts
+++ Frontend/project-tracker/src/app/app-routing.module.ts
@@ -1,13 +1,1 @@
-// import { ProjectFormComponent } from './project-form';
-// import { NgModule } from '@angular/core';
-// import { RouterModule, Routes } from '@angular/router';
-// const routes: Routes = [
-//   { path: 'project-form', component: ProjectFormComponent },
-// ];
-// @NgModule({
-//   imports: [RouterModule.forRoot(routes)],
-//   exports: [RouterModule]
-// })
-// export class aAppRoutingModule { }
-// export const routingComponents = [ProjectFormComponent]
 
