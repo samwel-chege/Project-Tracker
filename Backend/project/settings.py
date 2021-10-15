d707b39e0eb4830f7b54e1c1a7d00a57ad9af83d
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-10-15 12:43:29 +0300

    Update settings.py

--- Backend/project/settings.py
+++ Backend/project/settings.py
@@ -1,5 +1,3 @@
-
-
 from pathlib import Path
 import os
 import datetime
@@ -95,7 +93,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'trackerdb001',
+        'NAME': 'taliban_tracker',
         'USER': 'oscar',
         'PASSWORD': '123456789',
     }
