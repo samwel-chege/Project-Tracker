ab1ddbe8dafa6aa6be4ab1d4122fc2fef56dda49
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-10-16 14:12:37 +0300

    adds the obtain token url

--- Backend/Trackerapp/urls.py
+++ Backend/Trackerapp/urls.py
@@ -4,7 +4,7 @@ from .views import (
      RegisterView, VerifyEmail, LoginAPIView, PasswordTokenCheckAPI, 
      RequestPasswordResetEmail, SetNewPasswordAPIView, LogoutAPIView)
 from rest_framework import views
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 from django.conf.urls import url, include
 from .views import *
@@ -81,6 +81,7 @@ urlpatterns = [
     path('login/', LoginAPIView.as_view(), name='login'),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
     path('email-verify/', VerifyEmail.as_view(), name='email-verify'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('request-reset-email/', RequestPasswordResetEmail.as_view(), name='request-reset-email'),
     path('password-reset/<uidb64>/<token>/',PasswordTokenCheckAPI.as_view(), name='password-reset-confirmation' ),
