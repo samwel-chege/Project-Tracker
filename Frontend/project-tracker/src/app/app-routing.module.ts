60a83c1b3f0264e17a9e8738898954ba0ed0689c
samwel-chege <samwelchegeh09@gmail.com>
2021-10-17 11:53:53 +0300

    fetch projects api

--- Frontend/project-tracker/src/app/app-routing.module.ts
+++ Frontend/project-tracker/src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'projects', component: ProjectPageComponent },
 ];
 
 
--- Frontend/project-tracker/src/app/project-page/project-page.component.ts
+++ Frontend/project-tracker/src/app/project-page/project-page.component.ts
@@ -1,15 +1,23 @@
+import { HashLocationStrategy } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-
+import { ProjectService } from '../services/project.service';
 @Component({
   selector: 'app-project-page',
   templateUrl: './project-page.component.html',
   styleUrls: ['./project-page.component.css']
 })
 export class ProjectPageComponent implements OnInit {
-
-  constructor() { }
+  projects: any;
+  constructor(private projectService: ProjectService) { }
 
   ngOnInit(): void {
+    this.AllProjects();
+  }
+  AllProjects(){
+    this.projectService.getprojects().subscribe(projects => {
+      this.projects = projects;
+      console.log(this.projects)
+    })
   }
 
 }
