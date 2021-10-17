9300687ab3bd075da782e492b7d2c9a38493ddb8
samwel-chege <samwelchegeh09@gmail.com>
2021-10-17 22:14:42 +0300

    display student profile content

--- Frontend/project-tracker/src/app/app-routing.module.ts
+++ Frontend/project-tracker/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ProjectPageComponent } from './project-page/project-page.component';
+import { StudentProfileComponent } from './student-profile/student-profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'home', component: HomeComponent },
   { path: 'projects', component: ProjectPageComponent },
+  { path: 'profiles', component:StudentProfileComponent }
 ];
 
 
