fe8d3d662b697a89f44b122468f34234839ad6d0
samwel-chege <samwelchegeh09@gmail.com>
2021-10-17 12:01:59 +0300

    create a method to get student profiles

--- Frontend/project-tracker/src/app/services/student.service.ts
+++ Frontend/project-tracker/src/app/services/student.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class StudentService {
 
-  constructor() { }
+export class StudentProfileService{
+    api_link: string = "http://localhost:8000/";
+    constructor( private http: HttpClient) { }
+
+    getprofiles(){
+        return this.http.get(this.api_link + 'auth/api/students/');
+    }
+   
 }
