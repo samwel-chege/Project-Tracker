c0c5f4ae85c7f1c3309e77c75710283d30a1f895
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-10-20 11:54:57 +0300

    Remove custom user data

--- Frontend/project-tracker/src/app/student-profile/student-profile.component.ts
+++ Frontend/project-tracker/src/app/student-profile/student-profile.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentProfileService } from '../services/student.service';
-import { UserDataService } from '../services/userdata.service';
 import { AuthService } from '../services/auth.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -10,51 +9,19 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./student-profile.component.css']
 })
 export class StudentProfileComponent implements OnInit {
-  // currentUser: object = {};
-
-  // constructor(
-  //   public authService: AuthService,
-  //   private actRoute: ActivatedRoute
-  // ){
-  //   let id = this.actRoute.snapshot.paramMap.get('id');
-  //   this.authService.getUserProfile(id).subscribe(res=>{
-  //     this.currentUser = res.msg;
-  //   })
-  // }
-  // // students: any;
-  // // constructor( private studentProfileService: StudentProfileService) { }
-
-  // ngOnInit(): void {
-  //   // this.AllProfiles();
-  // }
-  // // AllProfiles(){
-  // //   this.studentProfileService.getprofiles().subscribe(students =>{
-  // //     this.students = students;
-  // //     console.log(this.students)
-  // //   })
-
-  // // }
 
 
   student: any;
-  user: any;
-  constructor( private studentProfileService: StudentProfileService, private UDService: UserDataService) { }
+  constructor( private studentProfileService: StudentProfileService) { }
 
   ngOnInit(): void {
     this.MyProfile();
-    this.MyData();
   }
+
   MyProfile(){
     this.studentProfileService.getProfile().subscribe(student =>{
       this.student = student;
       console.log(this.student)
-    })
-  }
-
-  MyData() {
-    this.UDService.getUser().subscribe(user => {
-      this.user = user;
-      console.log(this.user);
     })
   }
 }
