3548b5e7f104ccac332bf6adfcfd9ccb2514e27f
samwel-chege <samwelchegeh09@gmail.com>
2021-10-25 08:16:17 +0300

    make final changes

--- Frontend/project-tracker/src/app/uploadproject/uploadproject.component.ts
+++ Frontend/project-tracker/src/app/uploadproject/uploadproject.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { UploadprojectService } from './../services/uploadproject.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-uploadproject',
@@ -11,21 +12,61 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class UploadprojectComponent implements OnInit {
 
-    formData = new Proj('','','','','')
-    
+    form!: FormGroup
+    selectedFile: File;
+    description: string;
+    title: string
+    link: string
+    owner: string
+
+
 
   constructor(
     private UploadprojectService: UploadprojectService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder,
+
   ) { }
+  imageUpload(event:any){
+    this.selectedFile = event.target.files[0];
+    console.log(this.selectedFile)
+  }
+
+  descriptionChange(event:any){
+    this.description = event.target.value;
+    console.log(this.description)
+   }
+
+   titleChange(event:any){
+    this.title = event.target.value;
+    console.log(this.title)
+   }
+
+   linkChange(event:any){
+    this.link = event.target.value;
+    console.log(this.link)
+   }
+   ownerChange(event:any){
+    this.owner = event.target.value;
+    console.log(this.owner)
+   }
 
   ngOnInit(): void {
   }
   addProject(){
-  this.UploadprojectService.uploadProject(this.formData).subscribe(data =>{
+  const fd = new FormData();
+  fd.append('project_image', this.selectedFile)
+  fd.append('description', this.description)
+  fd.append('title', this.title)
+  fd.append('owner', this.owner)
+  fd.append('github_link', this.link)
+  this.UploadprojectService.uploadProject(fd).subscribe(data =>{
     console.log(data)
-    this.router.navigate(['projects']);
   })
+  this.router.navigate(['projects'])
+}
+refresh(){
+  window.location.reload();
 }
 }
